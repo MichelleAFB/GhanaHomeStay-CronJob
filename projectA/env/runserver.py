5fc151d59a9785ef84a680b37c6a937c8ef188b4
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-06-21 15:47:29 -0500

    fdsfds

--- projectA/env/runserver.py
+++ projectA/env/runserver.py
@@ -1,7 +1,12 @@
-import requests
-import json
 
-response=requests.get('https://ghanahomestayserver.onrender.com/')
 
-for data in response.json()['apps']:
-    print(data)
+print("hello")
+
+
+#import requests
+#import json
+
+#response=requests.get('https://ghanahomestayserver.onrender.com/')
+
+#for data in response.json()['apps']:
+  #  print(data)
