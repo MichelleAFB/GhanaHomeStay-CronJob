b749eb75632bfd482470ac132cbce6d6c185ecf6
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-07-24 13:23:23 -0500

    leetcode

--- projectA/runserver.py
+++ projectA/runserver.py
@@ -10,5 +10,7 @@ response=requests.get('https://ghanahomestayserver.onrender.com/')
 
 print(response)
 
+response2=requests.post('https://leetcodetracker.onrender.com/remove-duplicates')
+
 for data in response.json()['apps']:
     print(data)
