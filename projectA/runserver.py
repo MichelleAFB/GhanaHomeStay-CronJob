dc82a6c28c493e00e233328d386f6e23a5ecb977
MichelleAFB <64324539+MichelleAFB@users.noreply.github.com>
2023-07-30 21:27:07 -0500

    removed leetcode remove dups call

--- projectA/runserver.py
+++ projectA/runserver.py
@@ -10,7 +10,5 @@ response=requests.get('https://ghanahomestayserver.onrender.com/')
 
 print(response)
 
-response2=requests.post('https://leetcodetracker.onrender.com/remove-duplicates')
 
-for data in response.json()['apps']:
-    print(data)
+
